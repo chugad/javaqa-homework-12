869baf501f77cf679786d7794f659f79c366c99e
chugad <69381456+chugad@users.noreply.github.com>
2020-10-01 20:44:20 +0700

    Update Project: 1. Added Product.java; 2. Added ProductManager.java; 3. Added ProductRepository.java; 4. Added ProductManagerTest.java:   4.1 Added tests for method searchBy in ProductManager.java;

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,106 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Product item) {
+        repository.save(item);
+    }
+
+    public Product[] searchBy(String search) {
+
+        Product[] result = new Product[0];
+        for (Product product : repository.findAll()) {
+            if (matches(product, search) == true) {
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+    public boolean matches(Product product, String search) {
+
+        if (product instanceof Book) {
+            Book book = (Book) product;
+            if (book.getName().equalsIgnoreCase(search)) {
+                return true;
+            }
+            if (book.getAuthor().equalsIgnoreCase(search)) {
+                return true;
+            }
+            return false;
+        }
+        if (product instanceof Smartphone) {
+            Smartphone smartphone = (Smartphone) product;
+            if (smartphone.getName().equalsIgnoreCase(search)) {
+                return true;
+            }
+            if (smartphone.getProducer().equalsIgnoreCase(search)) {
+                return true;
+            }
+            return false;
+        }
+
+        return false;
+    }
+}
+
+//public class AfishaManager {
+//    int movieNumber = 10;
+//
+//    private AfishaRepository repository;
+//
+//    public  AfishaManager (AfishaRepository repository) {
+//        this.repository = repository;
+//    }
+//
+//    public AfishaManager (int movieNumber) {
+//        this.movieNumber = movieNumber;
+//    }
+//
+//    public void add(MoviesList index) {
+//        repository.save(index);
+//    }
+//
+//    public MoviesList[] getAll() {
+//        MoviesList[] movies = repository.findAll();
+//        int length = movies.length;
+//        if (length < this.movieNumber) {
+//            MoviesList[] result = new MoviesList[movies.length];
+//
+//            // перебираем массив в прямом порядке
+//            // но кладём в результаты в обратном
+//            for (int i = 0; i < result.length; i++) {
+//                int index = movies.length - i - 1;
+//                result[i] = movies[index];
+//            }
+//            return result;
+//        } else {
+//            MoviesList[] result = new MoviesList[this.movieNumber];
+//            // перебираем массив в прямом порядке
+//            // но кладём в результаты в обратном
+//            for (int i = 0; i < result.length; i++) {
+//                int index = movies.length - i - 1;
+//                result[i] = movies[index];
+//            }
+//            return result;
+//        }
+//    }
+//}
